0c4cf37f6144c3c6260e109f18a570a264e00cc9
Jeannie Torres <jeanniet89@gmail.com>
2022-07-01 03:53:09 -0400

    Projects area edits, sites responsive edits

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
-    let $btn = $('.project-area .button-group .button');
+    let $btns = $('.project-area .button-group button');
     
-    $btn.click(function(e){
+    $btns.click(function(e){
 
-        $('.project-area .button-group .button').removeClass('active');
+        $('.project-area .button-group button').removeClass('active');
             e.target.classList.add('active');
 
         let selector = $(e.target).attr('data-filter');
@@ -14,5 +14,30 @@ $(document).ready(function(){
 
         return false;
     })
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery:{enabled:true}
+      });
+
+      //sticky nav
+
+      let nav_offset_top = $('.header_area').height() + 50;
+      
+      function navbarFixed(){
+          if ($('.header_area').length){
+              $(window).scroll(function(){
+                  let scroll = $(window).scrollTop();
+                  if(scroll >= nav_offset_top){
+                      $('.site-main .header_area .main-menu').addClass('navbar_fixed');
+                  } else {
+                      $('.site-main .header_area .main-menu').removeClass('navbar_fixed');
+                  }
+              })
+          }
+      }
+      navbarFixed();
 });
 
